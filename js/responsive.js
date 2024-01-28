1d68fd54c603846a0438d11795172c1688777d05
Apollon977 <aybek028@gmail.com>
2024-01-28 22:03:11 +0300

    Last commit

--- js/responsive.js
+++ js/responsive.js
@@ -1,45 +1,75 @@
-const responsiveNavigation = document.querySelector('.responsive_navigation');
+const responsive_navigation = document.querySelector('.responsive_navigation');
 const overlay = document.querySelector('.overlay');
 const resposive_header = document.querySelector('.resposive_header');
 const action_button = document.querySelector('.action');
-
-window.addEventListener('scroll', function () {
-	let scrolled = window.scrollY;
-
-	// Проверяем, сколько пикселей прокручено и обновляем значение top
-	if (scrolled > 1) {
-		responsiveNavigation.style.top = '0';
-	} else {
-		responsiveNavigation.style.top = '68px';
-	}
-});
-
-action_button.addEventListener('click', () => {
-	responsiveNavigation.classList.toggle('active');
-	responsiveNavigation.classList.toggle(
-		'hidden',
-		!responsiveNavigation.classList.contains('active')
-	);
-
-	overlay.classList.toggle('active');
-	overlay.classList.toggle('hidden', !overlay.classList.contains('active'));
-
-	if (responsiveNavigation.classList.contains('active')) {
-		resposive_header.style.position = 'relative';
-		resposive_header.style.zIndex = '10';
-	}
-});
+const profile_button = document.getElementsByClassName(
+	'responsive_information-account'
+);
+const notif_modal = document.querySelector('.notif-modal');
+const notif_button = document.querySelector(
+	'.responsive_information-notification'
+);
+const modal_header = document.querySelector('.profile-modal');
 
 document.addEventListener('DOMContentLoaded', () => {
-	const button = document.getElementById('action_button');
-	const rect = document.getElementById('responsiveNavigation');
+	window.addEventListener('scroll', function () {
+		let scrolled = window.scrollY;
 
-	button.addEventListener('click', () => {
-		toggleTwoClasses(rect, 'active', 'hidden', 500);
+		if (scrolled > 1) {
+			responsive_navigation.style.top = '0';
+		} else {
+			responsive_navigation.style.top = '68px';
+		}
+	});
+
+	action_button.addEventListener('click', () => {
+		responsive_navigation.classList.toggle('active');
+		responsive_navigation.classList.toggle(
+			'hidden',
+			!responsive_navigation.classList.contains('active')
+		);
+		overlay.classList.toggle('active');
+		overlay.classList.toggle('hidden', !overlay.classList.contains('active'));
+		if (responsive_navigation.classList.contains('active')) {
+			resposive_header.style.position = 'relative';
+			resposive_header.style.zIndex = '10';
+		}
+	});
+
+	profile_button[0].addEventListener('click', () => {
+		document.querySelector('.account_arrows').classList.toggle('active');
 	});
 });
 
-function toggleTwoClasses(element, first, second, timeOfAnimation) {
+document.addEventListener('DOMContentLoaded', () => {
+	const button = document.getElementsByClassName('action_button');
+
+	button.addEventListener('click', () => {
+		navigation(responsive_navigation, 'active', 'hidden', 500);
+	});
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	const button_desktop = profile_button[0];
+	const button_mobile = profile_button[1];
+	const rect = modal_header;
+
+	button_desktop.addEventListener('click', () => {
+		navigation(rect, 'modal-visible', 'modal-hidden', 500);
+	});
+
+	button_mobile.addEventListener('click', () => {
+		navigation(rect, 'modal-visible', 'modal-hidden', 500);
+	});
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	notif_button.addEventListener('click', () => {
+		navigation(notif_modal, 'modal-visible', 'modal-hidden', 500);
+	});
+});
+
+function navigation(element, first, second, timeOfAnimation) {
 	if (!element.classList.contains(first)) {
 		element.classList.add(first);
 		element.classList.remove(second);
